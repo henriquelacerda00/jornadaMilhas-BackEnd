5dd006941ad8ce35835f16b59a49804e4a58f884
Henrique Lacerda <henriquelacerda69@gmail.com>
2025-07-07 13:31:52 -0300

    alterando bd

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Jornada Milhas')
-    .setDescription('Jornada  Milhas API')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  app.enableCors();
+  // ✅ Servir arquivos estáticos da pasta 'public'
+  // Exemplo: https://jornadamilhas-backend.onrender.com/public/veneza.png
+  app.use('/public', express.static(join(__dirname, '..', 'public')));
 
-  await app.listen(8080);
+  // ✅ Habilitar CORS para permitir requisições do front-end (local + Vercel)
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', // Front local (dev)
+      'https://alura-jornada-milhas.vercel.app', // Front produção (Vercel)
+    ],
+  });
+
+  // ✅ Iniciar o servidor na porta definida ou na 8080 por padrão
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
