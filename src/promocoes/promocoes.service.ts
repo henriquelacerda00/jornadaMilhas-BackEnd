2f00cb1b5f843b98aa0efd91d21c14714538f7e0
Henrique Lacerda <henriquelacerda69@gmail.com>
2025-07-07 12:33:12 -0300

    alterando url

--- src/promocoes/promocoes.service.ts
+++ src/promocoes/promocoes.service.ts
@@ -1,34 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Promocao } from './entities/promocao.entity';
 import { CreatePromocoeDto } from './dto/create-promocoe.dto';
 import { UpdatePromocoeDto } from './dto/update-promocoe.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Promocao } from './entities/promocao.entity';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class PromocoesService {
+  private readonly baseUrl = process.env.BASE_URL || 'http://localhost:8080';
+
   constructor(
     @InjectRepository(Promocao)
-    private repository: Repository<Promocao>,
+    private readonly repository: Repository<Promocao>,
   ) {}
 
-  create(createPromocoeDto: CreatePromocoeDto) {
+  async create(createPromocoeDto: CreatePromocoeDto) {
     return this.repository.save(createPromocoeDto);
   }
 
-  findAll() {
-    return this.repository.find();
+  async findAll(): Promise<Promocao[]> {
+    const promocoes = await this.repository.find();
+
+    return promocoes.map((promo) => ({
+      ...promo,
+      imagem: `${this.baseUrl}/public/${promo.imagem}`,
+    }));
   }
 
-  findOne(id: number) {
-    return this.repository.findOneBy({ id });
+  async findOne(id: number): Promise<Promocao | null> {
+    const promo = await this.repository.findOneBy({ id });
+
+    if (!promo) return null;
+
+    return {
+      ...promo,
+      imagem: `${this.baseUrl}/public/${promo.imagem}`,
+    };
   }
 
-  update(id: number, updatePromocoeDto: UpdatePromocoeDto) {
+  async update(id: number, updatePromocoeDto: UpdatePromocoeDto) {
     return this.repository.update(id, updatePromocoeDto);
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return this.repository.delete(id);
   }
 }
